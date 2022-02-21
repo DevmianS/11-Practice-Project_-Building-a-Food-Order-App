bbb7f9ede806b99e1ef5f1e77f4d94fe366ee326
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-02-21 09:39:43 +0100

    Init

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+      </header>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default Header;
